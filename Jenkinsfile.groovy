1010736c27ec7b459b58e0fe3f88d63e0b3cd5c7
Rob <brem_robert@hotmail.com>
2018-05-26 12:57:35 +0200

    [REFAC] use library function

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -21,30 +21,6 @@ podTemplate(label: 'mypod', containers: [
                 pipelineTriggers([])
         ])
 
-        def name = 'jenkins'
-
-        cleanWs()
-        stage('checkout') {
-            git url: "https://github.com/khinkali/${name}"
-        }
-
-        stage('build image & git tag & docker push') {
-            env.VERSION = semanticReleasing()
-            currentBuild.displayName = env.VERSION
-
-            sh "git config user.email \"jenkins@khinkali.ch\""
-            sh "git config user.name \"Jenkins\""
-            sh "git tag -a ${env.VERSION} -m \"${env.VERSION}\""
-            withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                sh "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/khinkali/${name}.git --tags"
-            }
-        }
-
-        stage('deploy') {
-            sh "sed -i -e 's/        image: khinkali\\/${name}:todo/        image: khinkali\\/${name}:${env.VERSION}/' kubeconfig.yml"
-            container('kubectl') {
-                sh "kubectl apply -f kubeconfig.yml"
-            }
-        }
+        deployInfra('jenkins', false)
     }
 }
